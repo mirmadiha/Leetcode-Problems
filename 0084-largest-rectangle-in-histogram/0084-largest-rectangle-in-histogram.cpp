ab3b41ae008fbbededc533e72a72aacdf213c5d4
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2024-12-30 19:47:01 +0530

    Time: 34 ms (42.71%), Space: 91.9 MB (11.17%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,57 +1,56 @@
 class Solution {
 public:
-    vector<int> nextSmallestElement(vector<int>& heights , int n){
-        stack<int>st;
-        vector<int>ans(n);
+
+    vector<int>nextSmallestElement(vector<int>heights , int n){
+        vector<int>answer(n);
+        stack<int>indexStack;
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
+            while(!indexStack.empty()  &&  heights[indexStack.top()]>=heights[i]){
+                indexStack.pop();
             }
-            if(st.empty()){
-                ans[i]=-1;
+            if(indexStack.empty()){
+                answer[i]=-1;
             }
-            else{
-                ans[i]=st.top();
+            else if(heights[indexStack.top()]<heights[i]){
+                answer[i]=indexStack.top();
             }
-            st.push(i);
+            indexStack.push(i);
         }
-        return ans;
+        return answer;
     }
 
-    vector<int> previousSmallestElement(vector<int>& heights , int n){
-        stack<int>st;
-        vector<int>ans(n);
-        for(int i=0;i<=n-1;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
-                st.pop();
+    vector<int>previousSmallestElement(vector<int>heights , int n){
+        vector<int>answer(n);
+        stack<int>indexStack;
+        for(int i=0;i<n;i++){
+            while(!indexStack.empty()  &&  heights[indexStack.top()]>=heights[i]){
+                indexStack.pop();
             }
-            if(st.empty()){
-                ans[i]=-1;
+            if(indexStack.empty()){
+                answer[i]=-1;
             }
-            else{
-                ans[i]=st.top();
+            else if(heights[indexStack.top()]<heights[i]){
+                answer[i]=indexStack.top();
             }
-            st.push(i);
+            indexStack.push(i);
         }
-        return ans;
+        return answer;
     }
 
     int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int>NSE(n);
-        vector<int>PSE(n);
-        int area=INT_MIN;
-        NSE=nextSmallestElement(heights,n);
-        PSE=previousSmallestElement(heights,n);
-        for(int i=0;i<heights.size();i++){
-            if(NSE[i]==-1){
-                NSE[i]=n;
-            }
-            int breadth=NSE[i]-PSE[i]-1;
-            int length=heights[i];
-            int newArea=length*breadth;
-            area=max(area,newArea);
+       int n=heights.size();
+       vector<int>NSE=nextSmallestElement(heights , n);
+       vector<int>PSE=previousSmallestElement(heights,n);
+       int area=INT_MIN;
+       for(int i=0;i<n;i++){
+        int length=heights[i];
+        if(NSE[i]<0){
+            NSE[i]=n;
         }
-        return area;
+        int breadth=NSE[i]-PSE[i]-1;
+        int newArea=length*breadth;
+        area=max(area , newArea);
+       } 
+       return area;
     }
 };
