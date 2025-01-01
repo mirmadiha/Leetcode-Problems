181ac201bf08c30d4ff412ed7e47082d45c6f46a
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-01-01 19:08:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (8.85%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    long long int binarySearch(int x){
-    int start=0;
-    int end=x;
-    long long int midPoint=start+(end-start)/2;
-    long long int ans=-1;
-
-while(start<=end){
-    long long int square=midPoint*midPoint;
-
-    if(square==x){
-        return midPoint;
-    }
-    else if(square>x){
-        end=midPoint-1;
-    }
-    else{
-        ans = midPoint;
-        start=midPoint+1;
-    }
-    midPoint=start+(end-start)/2;
-}
-return ans;
-}
     int mySqrt(int x) {
-        return binarySearch(x);
+        int start=0;
+        int end=x;
+        int ans=0;
+        long long int midPoint=start+(end-start)/2;
+        while(start<=end){
+            if(x==0){
+                return 0;
+            }
+            if(x==1){
+                return 1;
+            }
+            long long int sqrt=midPoint*midPoint;
+            if(sqrt==x){
+                return midPoint;
+            }
+            else if(sqrt<x){
+                ans=midPoint;
+                start=midPoint+1;
+            }
+            else{
+                end=midPoint-1;
+            }
+            midPoint=start+(end-start)/2;
+        }
+        return ans;
     }
 };
