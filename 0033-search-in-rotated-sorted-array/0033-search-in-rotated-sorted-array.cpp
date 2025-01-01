c774af6b431305b2602cbb85b4a1c225068f9d8a
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-01-01 18:29:57 +0530

    Time: 0 ms (100%), Space: 15.1 MB (39.74%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,49 +1,49 @@
 class Solution {
 public:
-    
-int pivotIndex(vector<int>& nums){
-    int start=0;
-    int end=nums.size()-1;
-    int midPoint=start+(end-start)/2;
-    while(start<end){
-        if(nums[midPoint]>=nums[0]){
-            start=midPoint+1;
+
+    int getPivot(vector<int>& nums){
+        int start=0;
+        int end=nums.size()-1;
+        int mid=start+(end-start)/2;
+        while(start<end){
+            if(nums[mid]>=nums[0]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+            mid=start+(end-start)/2;
+        }
+        return start;
+    }
+
+    int binarySearch(vector<int>& nums , int start ,int end,int target){
+        int mid=start+(end-start)/2;
+        while(start<=end){
+            if(target==nums[mid]){
+                return mid;
+            }
+            else if(target<nums[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+            mid=start+(end-start)/2;
+        }
+        return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        if(nums.size()==0){
+            return -1;
+        }
+        int pivotIndex=getPivot(nums);
+        if(nums[pivotIndex]<=target && nums[nums.size()-1]>=target){
+            return binarySearch(nums,pivotIndex,nums.size(),target);
         }
         else{
-            end=midPoint;
+            return binarySearch(nums,0,pivotIndex-1,target);
         }
-        midPoint=start+(end-start)/2;
-    }
-    return start;
-}
-
-
-int binarySearch(vector<int>& nums,int s,int e,int target){
-    int start=s;
-    int end=e;
-    int midPoint=start+(end-start)/2;
-    while(start<=end){
-    if(target==nums[midPoint]){
-        return midPoint;
-    }
-    else if(target<nums[midPoint]){
-        end=midPoint-1;
-    }
-    else{
-        start=midPoint+1;
-    }
-    midPoint=(start+end)/2;
-    }
-    return -1;
-}
-    
-    int search(vector<int>& nums, int target) {
-        int pivot=pivotIndex(nums);
-    if(nums[pivot]<=target && nums[nums.size()-1]>=target){
-        return binarySearch(nums,pivot,nums.size()-1,target);
-    }
-    else{
-        return binarySearch(nums,0,pivot-1,target);
-    }
     }
 };
