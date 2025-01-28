2c9723de693c7f976090a61ac832a80348050a0a
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-01-28 21:53:07 +0530

    Code Studio

---
+++ count-leaf-nodes-CodeStudio/count-leaf-nodes.cpp
@@ -0,0 +1,37 @@
+/**********************************************************
+
+    Following is the Binary Tree Node class structure:
+
+    template <typename T>
+    class BinaryTreeNode {
+      public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+                this -> data = data;
+                left = NULL;
+                right = NULL;
+        }
+    };
+    
+***********************************************************/
+void inorderTraversal(BinaryTreeNode<int>* root,int& count){
+    if(root==NULL){
+        return;
+    }
+
+    inorderTraversal(root->left,count);
+    if(root->left == NULL && root->right==NULL){
+        count++;
+    }
+    inorderTraversal(root->right,count); 
+}
+
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    // Write your code here.
+    int count = 0;
+    inorderTraversal(root,count);
+    return count;
+}
