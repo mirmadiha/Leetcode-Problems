7d252a5e10b592017b16a6fdbf1d4c51b239ba05
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-03-23 10:40:16 +0530

    Time: 0 ms (100%), Space: 147 MB (20.88%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -15,9 +15,17 @@ public:
         if(root==NULL){
             return 0;
         }
-        if(root->left==NULL) return minDepth(root->right)+1;
-        if(root->right==NULL) return minDepth(root->left)+1;
-        int answer=min(minDepth(root->left),minDepth(root->right))+1;
-        return answer;
+
+        if(root->left==NULL){
+            return minDepth(root->right)+1;
+        }
+
+        if(root->right==NULL){
+            return minDepth(root->left)+1;
+        }
+
+        int left=minDepth(root->left);
+        int right=minDepth(root->right);
+        return min(left,right)+1;
     }
 };
