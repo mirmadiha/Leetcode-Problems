ebd85c004bb05e6f05f981c8d72d27f2e96af84a
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-05-13 00:00:44 +0530

    GFG solution

---
+++ sum-of-the-longest-bloodline-of-a-tree/sum-of-the-longest-bloodline-of-a-tree.cpp
@@ -0,0 +1,56 @@
+// User function Template for C++
+
+/*
+structure of the node of the binary tree is as
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+  void solve(Node* root, int sum, int &maxSum, int len, int &maxLen) {
+        //base case
+        if( root == NULL ) {
+            
+            if(len > maxLen) 
+            {
+                maxLen = len;
+                maxSum = sum;
+            }
+            else if(len == maxLen) 
+            {
+                maxSum = max(sum, maxSum);
+            }
+            return;
+        }
+        
+        sum = sum + root->data;
+        
+        solve(root->left, sum, maxSum, len+1, maxLen);
+        solve(root->right, sum, maxSum, len+1, maxLen);
+
+    }
+    
+    int sumOfLongRootToLeafPath(Node *root) {
+        // code here
+        int len = 0;
+        int maxLen = 0;
+        
+        int sum = 0;
+        int maxSum = INT_MIN;
+        
+        solve(root, sum, maxSum, len, maxLen);
+        
+        return maxSum;
+    }
+};
