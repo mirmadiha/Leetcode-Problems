c3bc89f63eabbb11bd946a8d29cd977b3a2b08f3
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-08-19 23:58:07 +0530

    Time: 0 ms (100%), Space: 17.5 MB (52.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,55 +1,41 @@
 class Solution {
 public:
-    int firstOccurence(vector<int>& nums , int target){
-        int start=0;
-        int end=nums.size()-1;
-        int mid=start+(end-start)/2;
-        int result=-1;
-        while(start<=end){
-            if(nums[mid]==target){
-                result=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-            mid=start+(end-start)/2;
-        }
-        return result;
-    }
-
-    int lastOccurence(vector<int>& nums , int target){
-        int start=0;
-        int end=nums.size()-1;
-        int mid=start+(end-start)/2;
-        int result=-1;
-        while(start<=end){
-            if(nums[mid]==target){
-                result=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-            mid=start+(end-start)/2;
-        }
-        return result;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first=firstOccurence(nums,target);
-        int last=lastOccurence(nums,target);
-        if(first==-1 || last==-1){
-            return{-1,-1};
+        vector<int> ans(2, -1);
+        int start=0;
+        int end=nums.size()-1;
+        int midPoint=start+(end-start)/2;
+        while(start<=end){
+            if(nums[midPoint]==target){
+                ans[0]=midPoint;
+                end=midPoint-1;
+            }
+            else if(nums[midPoint]>target){
+                end=midPoint-1;
+            }
+            else{
+                start=midPoint+1;
+            }
+            midPoint=start+(end-start)/2;
         }
-        else{
-            return{first,last};
+
+
+        start=0;
+        end=nums.size()-1;
+        midPoint=start+(end-start)/2;
+        while(start<=end){
+            if(nums[midPoint]==target){
+                ans[1]=midPoint;
+                start=midPoint+1;
+            }
+            else if(nums[midPoint]>target){
+                end=midPoint-1;
+            }
+            else{
+                start=midPoint+1;
+            }
+            midPoint=start+(end-start)/2;
         }
+        return ans;
     }
 };
