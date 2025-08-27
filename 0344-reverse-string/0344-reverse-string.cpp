fe34492f3d0235859cdab432579551c333150c6a
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-08-27 23:56:15 +0530

    Time: 0 ms (100%), Space: 27.2 MB (51.27%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-
-    void recursiveReverse(vector<char>& s,int i,int size){
-        if(i>size-i-1){
-            return;
-        }
-        swap(s[i],s[size-i-1]);
-        recursiveReverse(s,++i,size);
-    }
-
     void reverseString(vector<char>& s) {
-        recursiveReverse(s,0,s.size());
+        int st=0, end=s.size()-1;
+        while(st<end){
+            swap(s[st],s[end]);
+            st++;
+            end--;
+        }
     }
 };
