1dd6bc986ab379e09fb7f91b0b26753a9223eecb
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-09-01 23:51:17 +0530

    Time: 3 ms (31.53%), Space: 9.8 MB (79.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,50 +1,22 @@
 class Solution {
-    char isLowercase(char ch){
-        char temp;
-        if(ch>='a'&&ch<='z' || ch>='0'&&ch<='9'){
-            return ch;
-        }
-        else{
-            temp=ch-'A'+'a';
-            return temp;
-        }
-    }
-    bool isValid(char ch){
-        if(ch>='a'&&ch<='z' || ch>='A'&&ch<='Z' || ch>='0'&&ch<='9'){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-    bool checkPalindrone(string temp){
-        int m=0;
-        int n=temp.length()-1;
-        while(m<=n){
-            if(temp[m]==temp[n]){
-                m++;
-                n--;
-            }
-            else{
-                return 0;
-            }
-        }
-        return 1;
-    }
 public:
     bool isPalindrome(string s) {
-        string temp;
-        int i=0;
-        int j=0;
-        while(j<s.length()){
-            if(isValid(s[j])){
-                temp.push_back(isLowercase(s[j]));
-                j++;
+        int left=0,right=s.size()-1;
+        while (left<right)
+        {
+            if(!isalnum(s[left])){
+                left++;
             }
-            else{
-                j++;
+            else if(!isalnum(s[right])){
+                right--;
+            }
+            else if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }else{
+                left++;
+                right--;
             }
         }
-        return checkPalindrone(temp);
+        return true;
     }
 };
