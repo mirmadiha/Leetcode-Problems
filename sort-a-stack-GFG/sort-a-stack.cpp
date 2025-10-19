92902951267013d8f7c9a082b439b268a6f7ef5b
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-10-19 14:46:02 +0530

    Refactor stack sorting into Solution class
    Refactored the sorting logic into a new class and method.

--- sort-a-stack-GFG/sort-a-stack.cpp
+++ sort-a-stack-GFG/sort-a-stack.cpp
@@ -1,38 +1,15 @@
-/*The structure of the class is
-class SortedStack{
-public:
-	stack<int> s;
-	void sort();
-};
-*/
-
-/* The below method sorts the stack s 
-you are required to complete the below method */
-void solveStack(stack<int>&s , int num){
-    if(s.empty() || (!s.empty()) && s.top()<num){
-        s.push(num);
-        return;
+class Solution {
+  public:
+    void sortStack(stack<int> &st) {
+        // code here
+        vector<int>num;
+        while(!st.empty()){
+            num.push_back(st.top());
+            st.pop();
+        }
+        sort(num.begin(),num.end());
+        for(int i=0;i<num.size();i++){
+            st.push(num[i]);
+        }
     }
-    
-    int x=s.top();
-    s.pop();
-    
-    solveStack(s,num);
-    s.push(x);
-}
-
-void SortedStack :: sort()
-{
-   //Your code here
-   if(s.empty()){
-       return;
-   }
-   
-   int num=s.top();
-   s.pop();
-   
-   sort();
-   
-   solveStack(s,num);
-   
-}
+};
