f38cb33ff7e0689db877fdd3f828bc25d017d607
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-10-19 15:45:29 +0530

    Simplify deleteMid function implementation
    Refactor deleteMid function to simplify logic and remove unnecessary recursion.

--- delete-mid-of-a-stack-GFG/delete-mid-of-a-stack.cpp
+++ delete-mid-of-a-stack-GFG/delete-mid-of-a-stack.cpp
@@ -1,25 +1,19 @@
 class Solution {
   public:
-    void solveStack(stack<int>& s,int count,int n){
-        int mid=(n/2);
-        if(count==mid){
-            s.pop();
-            return ;
-        }
-        
-        int num=s.top();
-        s.pop();
-        
-        solveStack(s,count+1,n);
-        
-        s.push(num);
-        
-    }
     // Function to delete middle element of a stack.
     void deleteMid(stack<int>& s) {
         // code here..
-        int count=0;
-        int n=s.size();
-        solveStack(s,count,n);
+        stack<int>st;
+        int midIndex=(s.size()+1)/2;
+        int iterations=(s.size()-midIndex);
+        for(int i=0;i<iterations;i++){
+            st.push(s.top());
+            s.pop();
+        }
+        s.pop();
+        while(!st.empty()){
+            s.push(st.top());
+            st.pop();
+        }
     }
 };
