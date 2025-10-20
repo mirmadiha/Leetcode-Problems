ba4b2bb26358f8838418f59377489d55d4f3b68c
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-10-20 11:15:06 +0530

    Refactor twoStacks class implementation
    Refactored twoStacks class to use fixed-size array and simplified push/pop logic.

--- two-stacks-in-an-array-GFG/two-stacks-in-an-array.cpp
+++ two-stacks-in-an-array-GFG/two-stacks-in-an-array.cpp
@@ -1,66 +1,48 @@
 class twoStacks {
   public:
-  int* arr;
-  int top1;
-  int top2;
-  int size;
+  int arr[100];
+  int index1=-1;
+  int n=100;
+  int index2=n;
 
-    twoStacks() {
-        size=100;
-        this->size=size;
-        arr=new int[size];
-        top1=-1;
-        top2=size;
-    }
+    twoStacks() {}
 
     // Function to push an integer into the stack1.
     void push1(int x) {
         // code here
-        if(top1 < top2 - 1){
-            top1++;
-            arr[top1]=x;
-        }
-        else{
-            cout<<"stack overflow"<<endl;
-        }
+        index1++;
+        arr[index1]=x;
+        
     }
 
     // Function to push an integer into the stack2.
     void push2(int x) {
         // code here
-        if(top1 < top2 - 1){
-            top2--;
-            arr[top2]=x;
-        }
-        else{
-            cout<<"stack overflow"<<endl;
-        }
+        index2--;
+        arr[index2]=x;
     }
 
     // Function to remove an element from top of the stack1.
+        
     int pop1() {
         // code here
-        if(top1>=0){
-            int y = arr[top1];
-            top1--;
-            return y;
-        }
-        else{
+        if(index1==-1){
             return -1;
         }
+        int num=arr[index1];
+        index1--;
+        return num;
     }
 
+        
     // Function to remove an element from top of the stack2.
     int pop2() {
         // code here
-        if(top2<size){
-            int y = arr[top2];
-            top2++; 
-            return y;
-        }
-        else{
+        if(index2==100){
             return -1;
         }
-        
+        int num=arr[index2];
+        index2++;
+        return num;
     }
 };
