1aab556fd6043c3088c1e803d8477b385ad0a8b1
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-11-23 17:41:41 +0530

    Refactor Deque implementation to use vector

--- implement-deque-CodeStudio/implement-deque.cpp
+++ implement-deque-CodeStudio/implement-deque.cpp
@@ -1,137 +1,119 @@
 #include <bits/stdc++.h> 
 class Deque
 {
-    int *arr;
-    int front;
-    int rear;
-    int size;
 public:
     // Initialize your data structure.
+    vector<int>dq;
+    int size,rear,front;
     Deque(int n)
     {
-        this->size=n;
-        arr=new int[size];
-        front=-1;
+        // Write your code here.
+        dq.resize(n);
+        size=n;
         rear=-1;
+        front=-1;
     }
 
     // Pushes 'X' in the front of the deque. Returns true if it gets pushed into the deque, and false otherwise.
     bool pushFront(int x)
     {
-        if(isFull()){
-            return false;
-        }
-        else if(front==-1){
-            front=rear=0;
-        }
-        else if(front==0 && rear!=size-1){
-            front=size-1;
+        // Write your code here.
+        if(isFull())return false;
+        if(isEmpty()){
+            front++;
+            rear++;
         }
         else{
-            front--;
+            front=((front-1)+size)%size;
         }
-        arr[front]=x;
+        dq[front]=x;
         return true;
     }
 
     // Pushes 'X' in the back of the deque. Returns true if it gets pushed into the deque, and false otherwise.
     bool pushRear(int x)
     {
-        if(isFull()){
-             return false;
-        }
-        else if(isEmpty()){
-            front=rear=0;
-        }
-        else if(rear==size-1 && front!=0){
-            rear=0;
-        }
-        else{
+        // Write your code here.
+        if(isFull())return false;
+        if(isEmpty()){
+            front++;
             rear++;
         }
-        arr[rear]=x;
+        else{
+            rear = (rear + 1) % size;
+
+        }
+        dq[rear]=x;
         return true;
     }
 
     // Pops an element from the front of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
     int popFront()
     {
-       if(isEmpty()){
-           return -1;
-       }
-       int ans=arr[front];
-       arr[front]=-1;
-       if(front==rear){
-           front=rear=-1;
-       }
-
-       else if(front==size-1){
-           front=0;
-       }
-       else{
-           front++;
-       }
-       return ans;
+        // Write your code here.
+        if(isEmpty()){
+            return -1;
+        }
+        int num=dq[front];
+        if (front == rear) {
+            front = rear = -1;
+        }
+        else{
+            front = (front + 1) % size;
+        }
+        return num;
     }
+
 
     // Pops an element from the back of the deque. Returns -1 if the deque is empty, otherwise returns the popped element.
     int popRear()
     {
+        // Write your code here.
         if(isEmpty()){
-           return -1;
-       }
-       int ans=arr[rear];
-
-       arr[rear]=-1;
-       if(front==rear){
-           front=rear=-1;
-       }
-       else if(rear==0){
-           rear=size-1;
-       }
-       else{
-           rear--;
-       }
-       return ans;
+            return -1;
+        }
+        int num=dq[rear];
+        if (front == rear) {
+            front = rear = -1;
+        }
+        else{
+            rear = (rear - 1 + size) % size;
+        }
+        return num;
     }
 
     // Returns the first element of the deque. If the deque is empty, it returns -1.
     int getFront()
     {
+        // Write your code here.
         if(isEmpty()){
             return -1;
         }
-        return arr[front];
+        return dq[front];
+        
     }
 
     // Returns the last element of the deque. If the deque is empty, it returns -1.
     int getRear()
     {
+        // Write your code here.
         if(isEmpty()){
             return -1;
         }
-        return arr[rear];
+        return dq[rear];
     }
 
     // Returns true if the deque is empty. Otherwise returns false.
     bool isEmpty()
     {
-        if(front==-1){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // Write your code here.
+        return front==-1;
     }
 
     // Returns true if the deque is full. Otherwise returns false.
     bool isFull()
     {
-        if(front==0 && rear==size-1 || (front!=0 && rear==(front-1)%(size-1))){
-            return true;
-        }
-        else{
-            return false;
-        }
+        // Write your code here.
+        return front==(rear+1)%size;
     }
 };
