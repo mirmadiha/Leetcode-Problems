797a75abd8cf47add03b825bfba845b563e57f68
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-11-26 13:32:11 +0530

    reverse array using recursion

--- reverse-the-array/reverse-an-array.cpp
+++ reverse-the-array/reverse-an-array.cpp
@@ -1,40 +1,19 @@
-/*Write a program in C++ to reverse an array from position M*/
-#include<iostream>
-#include<vector>
-using namespace std;
-
-
-vector<int> reverse(vector<int>arr,int m){
-    int start=m+1;
-    int end=arr.size()-1;
-    while(start<end){
-        swap(arr[start],arr[end]);
-        start++;
-        end--;
+class Solution {
+  public:
+  
+   void recursiveReverse(vector<int>&arr, int i,int j){
+       if(i>=j){
+           return;
+       }
+       swap(arr[i],arr[j]);
+       recursiveReverse(arr,i+1,j-1);
+   }
+  
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int i=0;
+        int j=n-1;
+        recursiveReverse(arr,i,j);
     }
-    return arr;
-}
-
-void print(vector<int>arr){
-    cout<<"The reversed array is:"<<endl;
-    for(int i=0;i<arr.size();i++){
-        cout<<arr[i]<<"  ";
-    }
-    cout<<endl;
-}
-
-
-
-int main()
-{
-    vector<int>arr;
-    arr.push_back(11);
-    arr.push_back(3);
-    arr.push_back(6);
-    arr.push_back(0);
-    arr.push_back(2);
-    arr.push_back(8);
-    int m=3;
-    vector<int>ans=reverse(arr,m);
-   print(ans);
-}
+};
