4a33c97086bfc88eb13b3239b374efe64c8de3a5
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2025-12-07 12:48:29 +0530

    Time: 551 ms (35.13%), Space: 262.1 MB (26.84%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,49 +1,48 @@
 class Solution {
 public:
-
-    void merge(vector<int>&nums, int low, int high, int mid){
-        vector<int>arr;
+    void merge(int low,int mid, int high,vector<int>&nums){
         int left=low;
         int right=mid+1;
-        if(low==high)return;
+        vector<int>temp;
         while(left<=mid && right<=high){
             if(nums[left]<nums[right]){
-                arr.push_back(nums[left]);
+                temp.push_back(nums[left]);
                 left++;
             }
             else{
-                arr.push_back(nums[right]);
+                temp.push_back(nums[right]);
                 right++;
             }
         }
 
         while(left<=mid){
-            arr.push_back(nums[left]);
+            temp.push_back(nums[left]);
             left++;
         }
-        while(right<=high){
-            arr.push_back(nums[right]);
+
+        while(right<=mid){
+            temp.push_back(nums[right]);
             right++;
         }
-        for(int i=low;i<=high;i++){
-            nums[i]=arr[i-low];
+
+        for(int i=0;i<temp.size();i++){
+            nums[low+i]=temp[i];
         }
     }
 
-    void mergeSort(vector<int>& nums,int low,int high){
+    void mergeSort(int low,int high,vector<int>&nums){
         int mid=(low+high)/2;
         if(low>=high){
             return;
         }
-        mergeSort(nums,low,mid);
-        mergeSort(nums,mid+1,high);
-        merge(nums,low,high,mid);
+        mergeSort(low,mid,nums);
+        mergeSort(mid+1,high,nums);
+        merge(low,mid,high,nums);
     }
-
     vector<int> sortArray(vector<int>& nums) {
         int low=0;
         int high=nums.size()-1;
-        mergeSort(nums,low,high);
+        mergeSort(low,high,nums);
         return nums;
     }
 };
