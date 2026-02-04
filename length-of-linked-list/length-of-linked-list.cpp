ef7d6a761536ab057d340e88869825f37750a0b7
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2026-02-04 23:56:07 +0530

    Add method to count nodes in linked list
    Implement getCount method to calculate the length of a linked list.

---
+++ length-of-linked-list/length-of-linked-list.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int getCount(Node* head) {
+        // Code here
+        Node* temp=head;
+        int count=0;
+       while(temp!=nullptr) {
+           count++;
+           temp=temp->next;
+       }
+       return count;
+    }
+};
