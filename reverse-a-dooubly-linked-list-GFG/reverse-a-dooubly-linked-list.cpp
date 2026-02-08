98b5bc626e8b267f701865705d6036932a6210b1
Madiha Aijaz <168270094+mirmadiha@users.noreply.github.com>
2026-02-08 22:14:25 +0530

    GFG solution
    Implement the reverse function for a doubly linked list.

---
+++ reverse-a-dooubly-linked-list-GFG/reverse-a-dooubly-linked-list.cpp
@@ -0,0 +1,35 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val) {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    Node *reverse(Node *head) {
+        // code here
+        if(head==nullptr || head->next==nullptr){
+            return head;
+        }
+        
+        Node* temp=head;
+        Node* before=nullptr;
+        while(temp!=nullptr){
+            Node* after=temp->next;
+            temp->next=before;
+            temp->prev=after;
+            before=temp;
+            temp=after;
+            after=temp->next;
+        }
+        return before;
+    }
+};
